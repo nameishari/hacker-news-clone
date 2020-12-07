5b5fa33d7d9d94187ba018324152ecc3bcef8d74
nameishari <hariharakumar88@gmail.com>
2020-12-07 12:15:47 +0100

    Refractor User component to use react hooks and functional components

--- app/components/User.js
+++ app/components/User.js
@@ -4,61 +4,69 @@ import { getUser, getStoriesByIds } from '../util/api'
 import Loading from './Loading'
 import StoriesList from './StoriesList'
 import MetaInfo from './MetaInfo'
-import { ThemeConsumer } from '../contexts/theme'
+import ThemeContext from '../contexts/theme'
 
-export default class User extends React.Component {
-    state = {
-        user: null,
-        stories: null
+function userReducer(state, action) {
+    switch (action.type) {
+        case 'fetch':
+            return {
+                user: null,
+                stories: null
+            }
+        case 'user-fetched':
+            return {
+                user: action.user,
+                stories: null
+            }
+        case 'stories-fetched':
+            return {
+                ...state,
+                stories: action.stories
+            }
+        default:
+            throw new Error('Not supported!')
     }
+}
 
-    componentDidMount() {
-        const { id } = queryString.parse(this.props.location.search)
-        this.handleFetch(id)
-    }
+export default function User() {
+    const [state, dispatch] = React.useReducer(userReducer, { user: null, story: null })
+    const theme = React.useContext(ThemeContext)
+    const { id } = queryString.parse(location.search)
 
-    handleFetch(id) {
-        this.setState({
-            user: null,
-            stories: null,
-        })
-
+    React.useEffect(() => {
+        dispatch({ type: 'fetch' })
         getUser(id)
-            .then((user) => this.setState({ user }))
-            .then(() => this.state.user.submitted)
+            .then((user) => {
+                dispatch({ type: 'user-fetched', user })
+                return user
+            })
+            .then((user) => user.submitted)
             .then((ids) => getStoriesByIds(ids))
-            .then((stories) => this.setState({ stories }))
-    }
+            .then((stories) => dispatch({ type: 'stories-fetched', stories }))
+    }, [id])
 
-    render() {
-        const { user, stories } = this.state
-        return (
-            <ThemeConsumer>
-                {
-                    ( theme ) => (
-                        <React.Fragment>
-                            {
-                                user === null
-                                    ? <Loading text='User is Loading' />
-                                    : <React.Fragment>
-                                        <p className={theme === 'light' ? 'title dark-color' : 'title light-color'}>{user.id}</p>
-                                        <MetaInfo by={user.id} time={user.created} karma={user.karma} />
-                                        <p className={theme === 'light' ? 'dark-color' : 'light-color'}>{user.about}</p>
-                                    </React.Fragment>
-                            }
+    const { user, stories } = state
+    return (
+        <React.Fragment>
+            {
+                user === null
+                    ? <Loading text='User is Loading' />
+                    : <React.Fragment>
+                        <p className={theme === 'light' ? 'title dark-color' : 'title light-color'}>{user.id}</p>
+                        <MetaInfo by={user.id} time={user.created} karma={user.karma} />
+                        <p className={theme === 'light' ? 'dark-color' : 'light-color'}>{user.about}</p>
+                    </React.Fragment>
+            }
 
-                            {
-                                user !== null && (stories === null
-                                    ? <Loading text='User stories are loading' />
-                                    : <React.Fragment>
-                                        <p className={theme === 'light' ? 'title dark-color' : 'title light-color'}>Posts</p>
-                                        <StoriesList stories={stories} />
-                                    </React.Fragment>)
-                            }
-                        </React.Fragment>
-                    )
-                }
-            </ThemeConsumer>
-        )
-    }
+            {
+                user !== null && (stories === null
+                    ? <Loading text='User stories are loading' />
+                    : <React.Fragment>
+                        <p className={theme === 'light' ? 'title dark-color' : 'title light-color'}>Posts</p>
+                        <StoriesList stories={stories} />
+                    </React.Fragment>)
+            }
+        </React.Fragment>
+    )
+
 }
