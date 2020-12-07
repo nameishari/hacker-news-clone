7cb6e3b9f2ed20adba164deaeddcb8b16d056903
nameishari <hariharakumar88@gmail.com>
2020-12-07 12:24:30 +0100

    Refractor Nav and Loading components to use react hooks

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,30 +1,26 @@
 import * as React from 'react'
 import { NavLink } from 'react-router-dom'
-import { ThemeConsumer } from '../contexts/theme'
+import ThemeContext from '../contexts/theme'
 
 const activeStyle = {
     color: 'rgb(187, 46, 31)'
 }
 
-export default function Nav({toggleTheme}) {
+export default function Nav({ toggleTheme }) {
+    const theme = React.useContext(ThemeContext)
     return (
-        <ThemeConsumer>
-            {(theme) => (
-                <nav className='row space-between'>
-                    <ul className='nav row'>
-                        <li>
-                            <NavLink exact activeStyle={activeStyle} to='/' className='nav-link'>Top</NavLink>
-                        </li>
-                        <li>
-                            <NavLink activeStyle={activeStyle} to='/new' className='nav-link'>New</NavLink>
-                        </li>
-                    </ul>
-                    <button onClick={toggleTheme} className='clear-btn' style={{fontSize: '30px'}}>
-                        {theme === 'light' ? '🔦' : '💡'}
-                    </button>
-                </nav>
-            )
-            }
-        </ThemeConsumer>
+        <nav className='row space-between'>
+            <ul className='nav row'>
+                <li>
+                    <NavLink exact activeStyle={activeStyle} to='/' className='nav-link'>Top</NavLink>
+                </li>
+                <li>
+                    <NavLink activeStyle={activeStyle} to='/new' className='nav-link'>New</NavLink>
+                </li>
+            </ul>
+            <button onClick={toggleTheme} className='clear-btn' style={{ fontSize: '30px' }}>
+                {theme === 'light' ? '🔦' : '💡'}
+            </button>
+        </nav>
     )
 }
--- app/contexts/theme.js
+++ app/contexts/theme.js
@@ -4,4 +4,3 @@ const ThemeContext = React.createContext()
 
 export default ThemeContext
 export const ThemeProvider = ThemeContext.Provider
-export const ThemeConsumer = ThemeContext.Consumer
